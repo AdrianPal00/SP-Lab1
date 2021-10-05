f10b99a0a6eaf44c8b70c3dd6ba6a3c92c678c3c
Adrian <adrian.pal00@e-uvt.ro>
2021-10-05 14:59:21 +0300

    Modified classes, constructors - getters/setters - implementation added

--- src/src/Book.java
+++ src/src/Book.java
@@ -1,6 +1,52 @@
 public class Book {
     private String title;
-    private String myText;
+    private String myText="";
 
+    public Book() {
+    }
 
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public Book(String title, String myText) {
+        this.title = title;
+        this.myText = myText;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMyText() {
+        return myText;
+    }
+
+    public void setMyText(String myText) {
+        this.myText = myText;
+    }
+
+    public void createNewParagraph(String str)
+    {
+        this.myText = this.myText +  "\n" + str;
+    }
+
+    public void createNewImage(String img)
+    {
+        this.myText = this.myText + "\n" + "This is my Image: " + img;
+    }
+
+    public void createNewTable(String table)
+    {
+        this.myText = this.myText + "\n" + "This is my Table: " + table;
+    }
+
+    public void print()
+    {
+        System.out.println(this.myText);
+    }
 }
